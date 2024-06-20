7b201829759d6b80219bd78b567ec46202157d04
Swapnanil Adhikary <117176842+SwapnanilAdhikary@users.noreply.github.com>
2024-06-20 21:33:44 +0530

    Add files via upload

---
+++ hidden-bread-54e2/src/index.ts
@@ -0,0 +1,12 @@
+export interface Env {
+
+}
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		 
+		return Response.json({
+			message: "you did not send a get request"
+		});
+		
+	},
+};
